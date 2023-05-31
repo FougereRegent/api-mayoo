17d2e1b61ba9f9aa40dad115d16b41567139ed59
damien venant-valéry <damien.venant@outlook.com>
2023-05-31 15:43:37 +0200

    Supressions des tests

--- src/test/java/com/mayoo/ApplicationTests.java
+++ src/test/java/com/mayoo/ApplicationTests.java
@@ -2,7 +2,7 @@ package com.mayoo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
 @SpringBootTest
 class ApplicationTests {
 
@@ -11,3 +11,4 @@ class ApplicationTests {
 	}
 
 }
+*/
