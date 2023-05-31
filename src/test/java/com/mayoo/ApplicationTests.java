f44a903bc68844a3d6601d167141076a123997ca
FouegereRegent <damien.venant@outlook.com>
2023-05-31 20:02:54 +0200

    Ajout des primitives de tests

--- src/test/java/com/mayoo/ApplicationTests.java
+++ src/test/java/com/mayoo/ApplicationTests.java
@@ -1,14 +1,30 @@
 package com.mayoo;
 
+import com.mayoo.Repository.UserRepository;
+import com.mayoo.Service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-/*
+
 @SpringBootTest
 class ApplicationTests {
+    @Mock
+    private UserRepository userRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @InjectMocks
+    private UserService userService;
 
+    @Test
+    public void test_create_user_check_email() {
+    }
+    @Test
+    public void test_create_user_check_password () {
+    }
+    @Test
+    public void test_create_user_check_username() {
+    }
+    @Test
+    public void test_create_user_check_exist() {
+    }
 }
-*/
