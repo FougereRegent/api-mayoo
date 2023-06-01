c029d1683202af561f0408ddb47a06bfc07b3dca
damien venant-valéry <damien.venant@outlook.com>
2023-06-01 12:00:17 +0200

    Je ne sais pas quoi mettre

--- src/main/java/com/mayoo/Service/FieldUserCheck/CheckCreatingUserBuilder.java
+++ src/main/java/com/mayoo/Service/FieldUserCheck/CheckCreatingUserBuilder.java
@@ -1,5 +1,6 @@
 package com.mayoo.Service.FieldUserCheck;
 
+import com.mayoo.Repository.UserRepository;
 import com.mayoo.Service.FieldUserCheck.Component.ExistComponent;
 import com.mayoo.Service.FieldUserCheck.Component.MailComponent;
 import com.mayoo.Service.FieldUserCheck.Component.UsernameComponent;
@@ -7,8 +8,8 @@ import com.mayoo.openapi.model.CreateUser;
 import org.apache.catalina.User;
 
 public class CheckCreatingUserBuilder {
-    public static IComponentCheck<CreateUser> builderResponsabilityCheckCreatingUser() {
-        IComponentCheck<CreateUser> checkExist = new ExistComponent();
+    public static IComponentCheck<CreateUser> builderResponsabilityCheckCreatingUser(UserRepository userRepository) {
+        IComponentCheck<CreateUser> checkExist = new ExistComponent(userRepository);
         IComponentCheck<CreateUser> checkEmail = new MailComponent();
         IComponentCheck<CreateUser> checkUsername = new UsernameComponent();
         IComponentCheck<CreateUser> checkPassword = new UsernameComponent();
--- src/main/java/com/mayoo/Service/FieldUserCheck/Component/ExistComponent.java
+++ src/main/java/com/mayoo/Service/FieldUserCheck/Component/ExistComponent.java
@@ -1,11 +1,18 @@
 package com.mayoo.Service.FieldUserCheck.Component;
 
+import com.mayoo.Repository.UserRepository;
 import com.mayoo.Service.FieldUserCheck.BaseComponent;
 import com.mayoo.openapi.model.CreateUser;
 
 public class ExistComponent extends BaseComponent<CreateUser> {
+    
+    private final UserRepository userRepository;
+    
+    public ExistComponent(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
     @Override
     public void execute(CreateUser user) {
-
     }
 }
