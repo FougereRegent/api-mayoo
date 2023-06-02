3d55cedd808f61a57f9519e9903f18d18327ae1a
damien venant-valéry <damien.venant@outlook.com>
2023-06-02 10:11:21 +0200

    Ajout de la fonctions de génération de salt

--- src/main/java/com/mayoo/Service/FieldUserCheck/Component/CreateUserComponent.java
+++ src/main/java/com/mayoo/Service/FieldUserCheck/Component/CreateUserComponent.java
@@ -1,15 +1,19 @@
 package com.mayoo.Service.FieldUserCheck.Component;
 
+import com.mayoo.DTO.UserMapper;
+import com.mayoo.Entity.UserEntity;
 import com.mayoo.Exceptions.CustomException;
 import com.mayoo.Repository.UserRepository;
 import com.mayoo.Service.FieldUserCheck.BaseComponent;
 import com.mayoo.openapi.model.CreateUser;
 import org.apache.catalina.User;
+import org.aspectj.apache.bcel.Constants;
+import org.aspectj.apache.bcel.generic.FieldOrMethod;
+import springfox.documentation.service.ApiListing;
 
 import java.util.Random;
 
 public class CreateUserComponent extends BaseComponent<CreateUser> {
-
     private final UserRepository userRepository;
     private final Random random;
     public CreateUserComponent(UserRepository userRepository, Random random) {
@@ -18,8 +22,25 @@ public class CreateUserComponent extends BaseComponent<CreateUser> {
     }
     @Override
     public void execute(CreateUser user) throws CustomException {
+        UserEntity userEntity = UserMapper.CreateUserEntityToUserObject(user);
 
         if(this.nextHandle != null)
             this.nextHandle.execute(user);
+    }
+    
+    private String generateSalt(int size) {
+        StringBuilder result = new StringBuilder();
+        final int ascii_a = 97;
+        final int ascii_z = 122;
+        for(int index = 0; index < size; index++) {
+            int generatedNumber = random.nextInt(ascii_a, ascii_z + 1);
+            char c = (char)generatedNumber;
+            result.append(c);
+        }
+        return result.toString();
+    }
+    
+    private String createHash(String chaine) {
+        return "";
     }
 }
