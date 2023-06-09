071461402016e8518e157591f7cb70821c5dee95
FouegereRegent <damien.venant@outlook.com>
2023-06-09 08:37:31 +0200

    Commencement de l'ajout de la jwt

--- src/main/java/com/mayoo/Config/SecurityConfig.java
+++ src/main/java/com/mayoo/Config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
--- src/main/java/com/mayoo/Entity/UserEntity.java
+++ src/main/java/com/mayoo/Entity/UserEntity.java
@@ -5,15 +5,18 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.annotation.processing.Generated;
+import java.util.Collection;
 
-@Getter
-@Setter
 @Builder
 @Entity
+@Getter
+@Setter
 @Table(name = "User")
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,4 +28,34 @@ public class UserEntity {
     
     @OneToOne
     private RightEnity right;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
