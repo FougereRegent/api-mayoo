b7fdbb4549523879e9353c8d6a25bba2d174678f
damien venant-valéry <damien.venant@outlook.com>
2023-06-10 13:25:23 +0200

    Ajout d'un nouveau fichier

---
+++ src/main/java/com/mayoo/Config/SecutiryConfiguration.java
@@ -0,0 +1,10 @@
+package com.mayoo.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecutiryConfiguration {
+    
+}
