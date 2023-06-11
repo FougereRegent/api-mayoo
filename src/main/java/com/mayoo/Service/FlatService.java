53570e90b0afe1c09605e7602765d78e714d0f94
damien venant-valéry <damien.venant@outlook.com>
2023-06-11 23:17:03 +0200

    Commencement du développement de quelque primitive

--- src/main/java/com/mayoo/Service/FlatService.java
+++ src/main/java/com/mayoo/Service/FlatService.java
@@ -1,31 +1,41 @@
 package com.mayoo.Service;
 
+import com.mayoo.DTO.FlatMapper;
+import com.mayoo.Entity.FlatEntity;
+import com.mayoo.Repository.FlatRepository;
 import com.mayoo.openapi.model.Flat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FlatService implements IFlatService{
+    private final FlatRepository flatRepository;
     @Override
     public List<Flat> allFlat() {
+        List<FlatEntity> flatEntities = flatRepository.findAll();
+        List<Flat> result = new ArrayList<>();
+        
+        flatEntities.forEach(flatEntity -> result.add(FlatMapper.flatEntityToFlat(flatEntity)));
+        return result;
+    }
+
+    @Override
+    public void addFlat(Flat flat) {
+    }
+
+    @Override
+    public Flat deleteFlat(Flat flat) {
         return null;
     }
 
     @Override
-    public void addFlat() {
-
-    }
-
-    @Override
-    public Flat deleteFlat() {
-        return null;
-    }
-
-    @Override
-    public Flat getDetailsFlat() {
-        return null;
+    public Flat getDetailsFlat(Long id) {
+        FlatEntity flat = flatRepository.findById(id)
+                .orElseThrow();
+        return FlatMapper.flatEntityToFlat(flat);
     }
 }
--- src/main/java/com/mayoo/Service/IFlatService.java
+++ src/main/java/com/mayoo/Service/IFlatService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface IFlatService {
     List<com.mayoo.openapi.model.Flat> allFlat();
-    void addFlat();
-    com.mayoo.openapi.model.Flat deleteFlat();
-    com.mayoo.openapi.model.Flat getDetailsFlat();
+    void addFlat(com.mayoo.openapi.model.Flat flat);
+    com.mayoo.openapi.model.Flat deleteFlat(com.mayoo.openapi.model.Flat flat);
+    com.mayoo.openapi.model.Flat getDetailsFlat(Long id);
 }
