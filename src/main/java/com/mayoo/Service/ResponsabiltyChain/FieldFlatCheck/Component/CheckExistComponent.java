2b7a12047e039818dbe6e10c8ecff670aadfe3ca
damien venant-valéry <damien.venant@outlook.com>
2023-06-12 09:16:54 +0200

    Commencement de l'implémentation de la chaine de responsabilité pour le flat

---
+++ src/main/java/com/mayoo/Service/ResponsabiltyChain/FieldFlatCheck/Component/CheckExistComponent.java
@@ -0,0 +1,12 @@
+package com.mayoo.Service.ResponsabiltyChain.FieldFlatCheck.Component;
+
+import com.mayoo.Exceptions.CustomException;
+import com.mayoo.Service.ResponsabiltyChain.BaseComponent;
+import com.mayoo.openapi.model.Flat;
+
+public class CheckExistComponent extends BaseComponent<Flat> {
+    @Override
+    public void execute(Flat user) throws CustomException {
+        
+    }
+}
---
+++ src/main/java/com/mayoo/Service/ResponsabiltyChain/FieldFlatCheck/Component/CheckNameComponent.java
@@ -0,0 +1,12 @@
+package com.mayoo.Service.ResponsabiltyChain.FieldFlatCheck.Component;
+
+import com.mayoo.Exceptions.CustomException;
+import com.mayoo.Service.ResponsabiltyChain.BaseComponent;
+import com.mayoo.openapi.model.Flat;
+
+public class CheckNameComponent extends BaseComponent<com.mayoo.openapi.model.Flat> {
+    @Override
+    public void execute(Flat user) throws CustomException {
+        
+    }
+}
