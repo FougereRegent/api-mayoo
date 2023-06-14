70fe8a16b9de3c4d6a1b789d061845e4b2cf821e
damien venant-valéry <damien.venant@outlook.com>
2023-06-14 11:03:45 +0200

    Création du builder

--- src/main/java/com/mayoo/Service/ResponsabiltyChain/FieldFlatCheck/CheckCreatingFlatBuilder.java
+++ src/main/java/com/mayoo/Service/ResponsabiltyChain/FieldFlatCheck/CheckCreatingFlatBuilder.java
@@ -1,7 +1,23 @@
 package com.mayoo.Service.ResponsabiltyChain.FieldFlatCheck;
 
+import com.mayoo.Repository.FlatRepository;
+import com.mayoo.Repository.PictogramRepository;
+import com.mayoo.Service.ResponsabiltyChain.FieldFlatCheck.Component.CheckExistComponent;
+import com.mayoo.Service.ResponsabiltyChain.FieldFlatCheck.Component.CheckNameComponent;
+import com.mayoo.Service.ResponsabiltyChain.FieldFlatCheck.Component.CheckPictogramsComponent;
+import com.mayoo.Service.ResponsabiltyChain.FieldFlatCheck.Component.CreateFlatComponent;
+import com.mayoo.Service.ResponsabiltyChain.IComponentCheck;
+
 public class CheckCreatingFlatBuilder {
-    public static void builder() {
+    public static IComponentCheck<com.mayoo.openapi.model.Flat> builder(FlatRepository flatRepository, PictogramRepository pictogramRepository) {
+        IComponentCheck<com.mayoo.openapi.model.Flat> checkNameComponent =  new CheckNameComponent();
+        IComponentCheck<com.mayoo.openapi.model.Flat> checkExistComponent = new CheckExistComponent(flatRepository);
+        IComponentCheck<com.mayoo.openapi.model.Flat> checkPictogramComponent = new CheckPictogramsComponent(pictogramRepository);
+        IComponentCheck<com.mayoo.openapi.model.Flat> createFlatComponenent = new CreateFlatComponent(flatRepository);
+        checkNameComponent.setNext(checkExistComponent);
+        checkExistComponent.setNext(checkPictogramComponent);
+        checkPictogramComponent.setNext(createFlatComponenent);
         
+        return checkNameComponent;
     }
 }
--- src/main/java/com/mayoo/Service/ResponsabiltyChain/FieldFlatCheck/Component/CreateFlatComponent.java
+++ src/main/java/com/mayoo/Service/ResponsabiltyChain/FieldFlatCheck/Component/CreateFlatComponent.java
@@ -2,17 +2,14 @@ package com.mayoo.Service.ResponsabiltyChain.FieldFlatCheck.Component;
 
 import com.mayoo.Exceptions.CustomException;
 import com.mayoo.Repository.FlatRepository;
-import com.mayoo.Repository.PictogramRepository;
 import com.mayoo.Service.ResponsabiltyChain.BaseComponent;
 import com.mayoo.openapi.model.Flat;
 
 public class CreateFlatComponent extends BaseComponent<com.mayoo.openapi.model.Flat> {
     private final FlatRepository flatRepository;
-    private final PictogramRepository pictogramRepository;
     
-    public CreateFlatComponent(FlatRepository flatRepository, PictogramRepository pictogramRepository) {
+    public CreateFlatComponent(FlatRepository flatRepository) {
         this.flatRepository = flatRepository;
-        this.pictogramRepository = pictogramRepository;
     }
     @Override
     public void execute(Flat user) throws CustomException {
