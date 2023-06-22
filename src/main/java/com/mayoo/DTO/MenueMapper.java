425d6e450ed0cb8e25431154439a0858f989db17
damien venant-valéry <damien.venant@outlook.com>
2023-06-22 20:09:23 +0200

    Commencement de l'implémentation du menue mapper

---
+++ src/main/java/com/mayoo/DTO/MenueMapper.java
@@ -0,0 +1,13 @@
+package com.mayoo.DTO;
+
+import com.mayoo.Entity.MenueEntity;
+
+public class MenueMapper {
+    public static com.mayoo.openapi.model.Menue MenueEntityToMenue(MenueEntity menue) {
+        return null; 
+    }
+    
+    public static MenueEntity MenueToMenuEntity(com.mayoo.openapi.model.Menue menue) {
+        return null; 
+    }
+}
